c89b8243c1da2ff3411e1d6e1bac88111a105820
John Oliver <jroliver@madisoncollege.edu>
2022-11-09 17:43:26 -0600

    Fixed database creation script to set most fields in the books table as not null

--- designDocuments/create-database.sql
+++ designDocuments/create-database.sql
@@ -6,21 +6,21 @@ DROP TABLE IF EXISTS books;
 
 -- tables
 -- Table: books
-CREATE TABLE IF NOT EXISTS books (
-                                     id bigint NOT NULL,
-                                     isbnTen varchar(20) NOT NULL,
-                                     isbnThirteen varchar(20) NOT NULL,
-                                     title varchar(100) NOT NULL,
-                                     author varchar(50) NOT NULL,
-                                     description text NOT NULL,
-                                     location varchar(50) NOT NULL,
-                                     publisher varchar(100) NOT NULL,
-                                     publishedDate date NOT NULL,
-                                     pageCount int NOT NULL,
-                                     language varchar(5) NOT NULL,
-                                     smallImageLink varchar(1024) NOT NULL,
-                                     medImageLink varchar(1024) NOT NULL,
-                                     CONSTRAINT books_pk PRIMARY KEY (id)
+CREATE TABLE books (
+                       id bigint NOT NULL,
+                       isbnTen varchar(20) NOT NULL,
+                       isbnThirteen varchar(20) NOT NULL,
+                       title varchar(100) NOT NULL,
+                       author varchar(50) NOT NULL,
+                       description text NOT NULL,
+                       location varchar(50) NOT NULL,
+                       publisher varchar(100) NOT NULL,
+                       publishedDate date NOT NULL,
+                       pageCount int NOT NULL,
+                       language varchar(5) NOT NULL,
+                       smallImageLink varchar(1024) NOT NULL,
+                       medImageLink varchar(1024) NOT NULL,
+                       CONSTRAINT books_pk PRIMARY KEY (id)
 );
 
 -- Table: users
